7ece70cf60068035baa2a09a6c8b7959f5367f9a
arthur <arthur.queiroz08@aluno.ifce.edu.br>
2024-01-15 19:46:38 -0300

    open closed principle

--- src/br/com/alura/rh/model/Funcionario.java
+++ src/br/com/alura/rh/model/Funcionario.java
@@ -1,10 +1,7 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-
-import br.com.alura.rh.ValidacaoException;
 
 public class Funcionario {
 
--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -1,13 +1,22 @@
+package br.com.alura.rh.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import br.com.alura.rh.model.Funcionario;
+
 public class ReajusteService {
 
-    public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
-        BigDecimal salarioAtual = funcionario.getSalario();
-		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
+	private List<Validacao> validacoes;
 
-        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+    public ReajusteService(List<Validacao> validacoes) {
+        this.validacoes = validacoes;
+    }
+
+    public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
+		validacoes.forEach(v -> v.validar(funcionario, aumento));
+
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
 		funcionario.atualizarSalario(salarioReajustado);
 	}
 }
